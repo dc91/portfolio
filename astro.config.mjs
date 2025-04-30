0a88b6eeca9e35ba47a24a3074a578e1c9d58c2d
dc91_ <dc91_@hotmail.com>
2025-04-30 22:01:07 +0200

    removed experimental svg

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  experimental: {
-    svg: true,
-  },
   site: 'https://dc91.github.io',
 });
